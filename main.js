d62d62e3067da686e2370f1020af9a92d3a4220c
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-10-20 17:50:45 -0400

    products changing

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 
-//create an empty array that will hold the shopping cart
+// -------------Jquery-----------
+$(document).ready(function() {
+
+
+
+$(".add-to-cart-button").click(function(event){
+    event.preventDefault();
+    var name=$(product-text).attr("data-name");
+    var price=Number($(product-text).attr("data-price"));
+
+    addItemToCart(name, price, 1);
+    console.log(cart);
+});
+
+
+$("#packs").change(function(){
+        if (this.value == "Pack_of_1"){
+            $("#singlepack").attr("src", "assets/original(gluten-free).png");
+        }
+        else if (this.value == "Pack_of_6"){
+            $("#singlepack").attr("src", "assets/Pack_of_6.jpg");
+        }
+        else{
+            $("#singlepack").attr("src", "assets/Pack_of_12.jpg");
+        }
+    })
+})
+
+
+// ------------------Javascript---------------------------
+// /create an empty array that will hold the shopping cart
 
 var cart=[];
 
@@ -59,13 +89,23 @@ function clearCart(){
 }
 
 
-console.log(cart.length);
-console.log(cart);
-removeAllItemsFromCart("Car");
-console.log(cart.length);
-console.log(cart);
-clearCart();
-console.log(cart);
+// -----------image manipulation-----------
+
+
+
+
+
+//function jsDropDown(imgid, newimg){
+//    document.getElementById(imgid).src="assets"+"/"+newimg+".jpg";
+//}
+
+// console.log(cart.length);
+// console.log(cart);
+// removeAllItemsFromCart("Car");
+// console.log(cart.length);
+// console.log(cart);
+// clearCart();
+// console.log(cart);
 //countCart() -> return total count
 
 //totalCart() -> return total cost
